f074b08adf54dbbc8dfc35fe96a5420921fc2381
J-McLemore <75950627+J-McLemore@users.noreply.github.com>
2021-01-28 20:57:13 -0600

    He we go

---
+++ wk2/exercises/2.1/arrays.js
@@ -0,0 +1,7 @@
+const fav = window.prompt(what is your favorite horror movie?");
+
+const myHorror = ['Sinister', 'The Conjuring', 'The Babadook', 'Insidious', 'Hereditary'];
+console.log(myHorror); 
+
+myHorror.push(fav);
+console.log(myHorror.toString());
